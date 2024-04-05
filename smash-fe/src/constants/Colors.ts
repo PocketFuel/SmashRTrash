f1ab76aee78494107d8ec70eacc86b40e13d3f3b
Hans Turner <91896581+PocketFuel@users.noreply.github.com>
2024-04-04 20:14:47 -0400

    Delete smash-fe directory

--- smash-fe/src/constants/Colors.ts
+++
@@ -1,4 +0,0 @@
-export const Yellow = "#DAA521"
-export const BorderColor = "#232222"
-export const BGColor = '#131313'
-export const SMTxt = "#808080"
--- smash-fe/src/constants/TokenConfig.ts
+++
@@ -1,2 +0,0 @@
-export const RBYTokenAddr = 'HD7TPfz7QwaoTGf46ifxiqAWc1NVxBBJ9mpRQbqtwamQ'
-export const treasuryToken = '5CneCDMkg5KyZAPxkQjWw3rPAVEqYDXpLbNLRuZmbdw7'
